8af6f815f135834674136e4fd52f838b1841ae77
mfbrewster <michael.f.brewster@gmail.com>
2016-02-29 10:24:49 -0500

    Building in logic of windows closing and opening

--- assets/scripts/jquery-listeners.js
+++ assets/scripts/jquery-listeners.js
@@ -2,14 +2,32 @@
 
 const userFunc = require('./user-functions');
 
-const hideForms = function(e) {
-    $('user-form').hide();
-}
+const hideForms = function() {
+  $('.content').hide();
+};
+
+const showLogIn = function() {
+  $('#sign-in-window').show();
+};
+
+const switchToWindow = {
+  signUp: function() {
+    hideForms();
+    $('#sign-up-window').show();
+  },
+  signIn: function() {
+    hideForms();
+    $('#sign-in-window').show();
+  },
+};
 
 let userHandler = function() {
   $('#sign-up').on('submit', userFunc.signUp);
+  $('#sign-in').on('submit', userFunc.signIn);
+  $('#not-yet-user').on('click', switchToWindow.signUp);
 };
 
 $(document).ready(() => {
   userHandler();
+  switchToWindow.signIn();
 });
--- assets/scripts/user-functions.js
+++ assets/scripts/user-functions.js
@@ -1,10 +1,12 @@
 'use strict';
 
+const baseUrl = 'http://localhost:3000';
+
 const signUp = function signUp(e) {
   e.preventDefault();
   var formData = new FormData(e.target);
   $.ajax({
-    url: 'http://localhost:3000/sign-up',
+    url: baseUrl + '/sign-up',
     method: 'POST',
     contentType: false,
     processData: false,
@@ -16,6 +18,24 @@ const signUp = function signUp(e) {
   });
 };
 
+const signIn = function signUp(e) {
+  e.preventDefault();
+  var formData = new FormData(e.target);
+  $.ajax({
+    url: baseUrl + '/sign-in',
+    method: 'POST',
+    contentType: false,
+    processData: false,
+    data: formData
+  }).done(function(data) {
+    console.log(data);
+  }).fail(function(jqxhr) {
+    console.error(jqxhr);
+  });
+};
+
+
 module.exports = {
   signUp,
+  signIn,
 };
